dad09aaf0cd69a286c72d1dfb4ca6149c347ab89
FatSquare <legitcsgo5@gmail.com>
2021-12-24 13:01:57 +0000

    rm config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-host="localhost"
-username="username"
-password="password"
-database="database"
-
-guide_examples=True
-allow_clear_console=False
-sqli_warning=True
-config_path="" #nothing means current direcotry
