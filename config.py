459d1a90d5d8bb366157ce7dc0f7b9dd8590f5e0
Squar3 <69780128+FatSquare@users.noreply.github.com>
2021-12-24 13:03:30 +0000

    default config

---
+++ config.py
@@ -0,0 +1,10 @@
+#U
+host="localhost"
+username="username"
+password="password"
+database="database"
+
+guide_examples=True
+allow_clear_console=False
+sqli_warning=True
+config_path="" #nothing means current direcotry
